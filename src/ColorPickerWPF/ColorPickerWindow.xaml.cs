8c4ad010467c4fd3b079dfb6ab8c35094c21fc5c
brycecot <bcotner2@friedrich.com>
2017-08-16 14:53:52 -0500

    Initial commit

---
+++ src/ColorPickerWPF/ColorPickerWindow.xaml.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+using ColorPickerWPF.Code;
+using ColorPickerWPF.Properties;
+
+namespace ColorPickerWPF
+{
+    /// <summary>
+    /// Interaction logic for ColorPickerWindow.xaml
+    /// </summary>
+    public partial class ColorPickerWindow : Window
+    {
+        protected readonly int WidthMax = 574;
+        protected readonly int WidthMin = 342;
+        protected bool SimpleMode { get; set; }
+
+
+
+        public ColorPickerWindow()
+        {
+            InitializeComponent();
+
+            FilenameTextBox.Text = Settings.Default.DefaultColorPaletteFilename;
+        }
+
+
+        public static bool ShowDialog(out Color color, Color colorin, ColorPickerDialogOptions flags = ColorPickerDialogOptions.None, string customPaletteName = null)
+        {
+            var instance = new ColorPickerWindow();
+            color = colorin;
+            instance.ColorPicker.Color = colorin;
+            instance.ColorPicker.SetColor(colorin);
+
+            if ((flags & ColorPickerDialogOptions.SimpleView) == ColorPickerDialogOptions.SimpleView)
+            {
+                instance.ToggleSimpleAdvancedView();
+            }
+
+            if ((flags & ColorPickerDialogOptions.LoadCustomPalette) == ColorPickerDialogOptions.LoadCustomPalette)
+            {
+                if (!String.IsNullOrEmpty(customPaletteName))
+                {
+                    instance.ColorPicker.LoadCustomPalette(customPaletteName);
+                    instance.FilenameTextBox.Text = customPaletteName;
+                }
+                else
+                {
+                    instance.ColorPicker.LoadDefaultCustomPalette();
+                }
+            }
+
+            var result = instance.ShowDialog();
+            if (result.HasValue && result.Value)
+            {
+                color = instance.ColorPicker.Color;
+                return true;
+            }
+
+            return false;
+
+        }
+        public static bool ShowDialog(out Color color, ColorPickerDialogOptions flags = ColorPickerDialogOptions.None, string customPaletteName = null)
+        {
+            var instance = new ColorPickerWindow();
+            color = instance.ColorPicker.Color;
+
+            if ((flags & ColorPickerDialogOptions.SimpleView) == ColorPickerDialogOptions.SimpleView)
+            {
+                instance.ToggleSimpleAdvancedView();
+            }
+
+            if ((flags & ColorPickerDialogOptions.LoadCustomPalette) == ColorPickerDialogOptions.LoadCustomPalette)
+            {
+                if (!String.IsNullOrEmpty(customPaletteName))
+                {
+                    instance.ColorPicker.LoadCustomPalette(customPaletteName);
+                    instance.FilenameTextBox.Text = customPaletteName;
+                }
+                else
+                {
+                    instance.ColorPicker.LoadDefaultCustomPalette();
+                }
+            }
+
+            var result = instance.ShowDialog();
+            if (result.HasValue && result.Value)
+            {
+                color = instance.ColorPicker.Color;
+                return true;
+            }
+
+            return false;
+        }
+
+
+
+
+
+        private void OKButton_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+            Hide();
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+            Hide();
+        }
+
+        private void MinMaxViewButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (SimpleMode)
+            {
+                SimpleMode = false;
+                MinMaxViewButton.Content = "<< Collapse";
+                Width = WidthMax;
+            }
+            else
+            {
+                SimpleMode = true;
+                MinMaxViewButton.Content = "Expand >>";
+                Width = WidthMin;
+            }
+        }
+
+        public void ToggleSimpleAdvancedView()
+        {
+            if (SimpleMode)
+            {
+                SimpleMode = false;
+                MinMaxViewButton.Content = "<< Collapse";
+                Width = WidthMax;
+            }
+            else
+            {
+                SimpleMode = true;
+                MinMaxViewButton.Content = "Expand >>";
+                Width = WidthMin;
+            }
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            var text = FilenameTextBox.Text;
+            if (!String.IsNullOrEmpty(text))
+            {
+                ColorPicker.SaveCustomPalette(text);
+            }
+        }
+
+        private void LoadButton_Click(object sender, RoutedEventArgs e)
+        {
+            var text = FilenameTextBox.Text;
+            if (!String.IsNullOrEmpty(text))
+            {
+                ColorPicker.LoadCustomPalette(text);
+            }
+        }
+    }
+}
